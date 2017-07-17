2eb744f7a58411ff26a7cc274d644719fda58aa6
BryanPlum <bryanplumm@gmail.com>
2017-07-17 14:47:00 -0300

    Code jQuery

---
+++ bg_color_mouseenter.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	HoverMenu()
+})
+
+function HoverMenu(){
+	$("#menu ul li a").mouseenter(function(){
+		$(this).css("background-color", "yellow")
+	})
+	 $("#menu ul li a").mouseleave(function(){
+        $(this).css("background-color", "#fff");
+    });
+}
